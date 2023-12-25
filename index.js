42b1da8909405a9af544b46b12ed71cdd2d09b75
Israt Jahan <isratjahanpanna87@gmail.com>
2023-12-25 23:34:29 +0600

    create task

--- index.js
+++ index.js
@@ -5,13 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 4200
 // midelwire
-app.use(cors(
-
-    {
-        origin: ['https://geolite-client-site.web.app'],
-        credentials:true
-      }
-));
+app.use(cors());
 app.use(express.json());
 
 // mongodb connections 
@@ -60,7 +54,74 @@ async function run() {
       res.send(result)
     })
 
-  
+    // added new task
+const taskCollection = client.db("sccTechnovision").collection("addtask");
+
+app.get('/tasks' , async(req , res ) => {
+  const cursor = taskCollection.find();
+  const result = await cursor.toArray();
+  res.send(result);
+})
+
+// add new tasks
+app.post('/tasks', async (req , res) =>{
+    const addedNewTask = req.body ;
+    console.log(addedNewTask);
+    const result = await taskCollection.insertOne(addedNewTask);
+    res.send(result)
+})
+
+  // // Update task by ID 
+  app.get('/tasks/:id' , async(req , res ) => {
+    const id =req.params.id ;
+  const query = {_id : new ObjectId (id)}
+  const result = await taskCollection.findOne(query)
+  res.send(result)
+  })
+
+  app.put('/tasks/:id',async(req , res ) =>{
+    const id = req.params.id;
+    const filter = {_id : new ObjectId (id)}
+    const updatePost = req.body;
+    const tasks = {
+      $set:{
+        creatorEmail:updatePost.creatorEmail,
+        taskTitle:updatePost.taskTitle,
+        deadline:updatePost.deadline,
+        description:updatePost.description,
+        priority:updatePost.priority
+        
+      }
+    }
+    const result = await taskCollection.updateOne(filter,jobs);
+    res.send(result);
+  })
+
+
+
+
+app.post('/tasks', async (req, res) => {
+try {
+    const { creatorEmail, taskTitle, deadline, description, priority } = req.body;
+
+    // Use these values to create the document you want to insert
+    const taskDocument = {
+      creatorEmail,
+      taskTitle,
+        deadline,
+        description,
+        priority
+    };
+
+    const result = await taskCollection.insertOne(jobDocument);
+    res.send(result);
+    
+} catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+}
+});
+
   run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('sccTechnovision ')
